5c67488c4d931c1577b95cb55360b2507190d91d
Armageddonas <icsd11175@icsd.aegean.gr>
2016-01-08 02:30:37 +0200

    Minor corrections

--- app/src/main/java/com/zeuss_works/armageddonas/supersimplelocationsense/MapsActivity.java
+++ app/src/main/java/com/zeuss_works/armageddonas/supersimplelocationsense/MapsActivity.java
@@ -1,17 +1,20 @@
 package com.zeuss_works.armageddonas.supersimplelocationsense;
 
+import android.Manifest;
 import android.content.Context;
 import android.location.Address;
 import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.content.pm.PackageManager;
 import android.util.Log;
 import android.widget.Toast;
 
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -40,7 +43,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
-        //region location declare
+
         // Acquire a reference to the system Location Manager
         locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
 
@@ -69,10 +72,9 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             }
         };
 
-
         try {
             // Register the listener with the Location Manager to receive location updates
-            locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 5000, 15, locationListener);
+            locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 5000, 0f, locationListener);
         } catch (SecurityException ex) {
             Toast.makeText(getBaseContext(), "Permission Problem",
                     Toast.LENGTH_SHORT).show();
@@ -80,15 +82,13 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         }
 
         try {
-            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 15, locationListener);
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 3000, 0f, locationListener);
 
         } catch (SecurityException ex) {
             Toast.makeText(getBaseContext(), "Permission Problem",
                     Toast.LENGTH_SHORT).show();
             Log.v(TAG, "Permission problem");
         }
-
-        //endregion
     }
 
     public void refreshLocation(Location location) {
@@ -119,10 +119,29 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             e.printStackTrace(); // getFromLocation() may sometimes fail
         }
         //endregion
+//        location = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        } else {
+            Location loc = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+            if (loc != null) {
+                refreshLocation(loc);
+                Log.v(TAG, "Got location");
+            }
+        }
     }
 }
